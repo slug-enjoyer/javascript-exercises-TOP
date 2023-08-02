d4d4cf289c5a2429e06a9c0b124718db540cb7da
slug-enjoyer <81844498+slug-enjoyer@users.noreply.github.com>
2023-08-01 22:34:24 -0700

    finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
-};
+    let stringHolder = ''
 
+    if (num >= 0){
+        for (let i=1; i <= num; i++){
+        stringHolder += string;
+        }
+        return stringHolder;
+    }
+
+
+    else if (num < 0){
+        return 'ERROR';
+    }
+
+}
 // Do not edit below this line
 module.exports = repeatString;
