933048028479fd72b2678d415146cfb276dd41cf
slug-enjoyer <81844498+slug-enjoyer@users.noreply.github.com>
2023-08-03 23:29:28 -0700

    finished lesson 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(usrString) {
 
+    // const usrString = prompt("Enter a string to be reversed: ");
+    const stringCounterInit = usrString.length;
+    let stringCounter = usrString.length;
+    let newString = "";
+
+    for (let i = 1; i <= (stringCounterInit+1); i++){
+        newString = newString+usrString.charAt(stringCounter);
+        stringCounter--;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
