bca822f47102301647ddb4e67d30cb63c18e84ac
slug-enjoyer <81844498+slug-enjoyer@users.noreply.github.com>
2023-08-06 02:18:14 -0700

    Lesson 04 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(userArray, ...removeIndex) {
+    const result = userArray.filter(item => !removeIndex.includes(item));
+    return result;
+    
+}
 // Do not edit below this line
 module.exports = removeFromArray;
