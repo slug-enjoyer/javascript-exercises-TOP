ab3704afc70dcf9276d5f99885158fcc196595af
slug-enjoyer <81844498+slug-enjoyer@users.noreply.github.com>
2023-08-23 01:46:37 -0700

    Completed lesson 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let celsius = ((fTemp-32)/1.8);
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fahrenheit = ((cTemp*1.8)+32);
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((total, current)=> total + current,0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let sum = 1;
+  for (let i = 0; i < args.length; i++){
+    sum *= args[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let sum = 1
+  for(let i = 1; i <= x; i++){
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
