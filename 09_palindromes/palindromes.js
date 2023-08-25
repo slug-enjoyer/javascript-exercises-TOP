63529b4010587b67c18269decd9eff71b63b9d4d
slug-enjoyer <81844498+slug-enjoyer@users.noreply.github.com>
2023-08-24 22:13:57 -0700

    started work on 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    const revString = string.split("").reverse().join("");
+    if (revString === string){
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
