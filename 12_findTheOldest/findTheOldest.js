dba15b879a973b3b143f5c55bb8e18c1018bba5f
slug-enjoyer <81844498+slug-enjoyer@users.noreply.github.com>
2023-10-23 00:48:45 -0700

    initial program

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+ const people = peopleArray;  
+    const currentYear = new Date().getFullYear();
 
+    const oldestPerson = people.reduce((oldest, current) => {
+      const calculateAge = person => {
+        if (person.deathYear === undefined || person.deathYear === 0) {
+          return currentYear - person.birthYear;
+        }
+        return person.deathYear - person.birthYear;
+      };
+    
+      const oldestAge = calculateAge(oldest);
+      const currentAge = calculateAge(current);
+    
+      if (currentAge > oldestAge) {
+        return current;
+      } 
+      else {
+        return oldest;
+      }
+    });
 };
 
 // Do not edit below this line
